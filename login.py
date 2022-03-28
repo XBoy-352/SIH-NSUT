f82c23bc5687dcb03a3840a3d9bb934bb65ce406
Aalok-NSUT <96689398+Aalok-NSUT@users.noreply.github.com>
2022-03-28 13:41:23 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,29 @@
+import mysql.connector as sql
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+mydb = sql.connect(host = "localhost", database = "aadhaar", user = "root", password = "buff_bl1tz_")
+db = mydb.cursor()
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        roll_no = request.form['roll_no']
+        aadhaar = request.form['aadhaar']
+        db.execute(''' INSERT INTO authentication VALUES(%s,%s)''',(roll_no,aadhaar))
+        mydb.commit()
+        db.close()
+        return redirect(url_for("output"))
+
+    else:
+        return render_template('login.html')
+
+@app.route('/output', methods = ['POST', 'GET'])
+def output():
+    if request.method == 'POST':
+        return render_template('output.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+mydb.close()
