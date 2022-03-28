b17177fe85ec5c6840c34345b5db73a3dbd6c5e2
Aalok-NSUT <96689398+Aalok-NSUT@users.noreply.github.com>
2022-03-28 14:34:34 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -3,11 +3,15 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-mydb = sql.connect(host = "localhost", database = "aadhaar", user = "root", password = "buff_bl1tz_")
+mydb = sql.connect(host = "localhost", database = "aadhaar", user = "root", password = "vanssh1234aalok?")
 db = mydb.cursor()
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
+    return render_template('login.html')
+
+@app.route('/output', methods = ['POST', 'GET'])
+def output():
     if request.method == 'POST':
         roll_no = request.form['roll_no']
         aadhaar = request.form['aadhaar']
@@ -15,13 +19,7 @@ def login():
         mydb.commit()
         db.close()
         return redirect(url_for("output"))
-
     else:
-        return render_template('login.html')
-
-@app.route('/output', methods = ['POST', 'GET'])
-def output():
-    if request.method == 'POST':
         return render_template('output.html')
 
 if __name__ == '__main__':
